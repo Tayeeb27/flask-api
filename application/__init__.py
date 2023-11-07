0213879d12e3547201616b32daa5d4dc70f127ad
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2023-11-07 15:11:03 +0000

    Hid DB URL

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tygvxvry:ZS4r_gYH9GZPgVoreaVYPSXayzdwwGE2@trumpet.db.elephantsql.com/tygvxvry'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
 from application import routes
